3756a6eaf8be655acc0657a782f71b91ce0ea92b
nikosmpantekas <nikosmpantekas2@gmail.com>
2025-05-15 03:16:21 +0300

    Fix authentication by adding matchPassword method to userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
   {
@@ -39,5 +40,25 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Method to compare entered password with hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Middleware to hash password before saving user
+userSchema.pre('save', async function (next) {
+  // Only hash the password if it's modified (or new)
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  // Generate salt
+  const salt = await bcrypt.genSalt(10);
+  
+  // Hash the password with the salt
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 module.exports = mongoose.model('User', userSchema);
