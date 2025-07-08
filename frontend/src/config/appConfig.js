9ea2f0ed85d04deb52efdd20d19534bf6233f108
nikosmpantekas <nikosmpantekas2@gmail.com>
2025-07-08 12:47:27 +0300

    V 1.6.1.185 , Fixing http/https force requests to api

--- frontend/src/config/appConfig.js
+++ frontend/src/config/appConfig.js
@@ -5,7 +5,7 @@
 // App version (NOTIFICATION SYSTEM COMPLETELY REMOVED)
 export const appConfig = {
   name: 'GradeBook',
-  version: '1.6.0.184',
+  version: '1.6.0.185',
   author: 'GradeBook Team'
 };
 
