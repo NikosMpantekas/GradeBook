a613980983c739c1c7eceb3eb6a2363f909ad8f8
nikosmpantekas <nikosmpantekas2@gmail.com>
2025-07-08 13:29:54 +0300

    V 1.6.1.190 , Fixing http/https force requests to api

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -1,14 +1,34 @@
 // This service worker file is completely empty to disable CRA's service worker generation
-// Skip workbox precaching
-self.__WB_DISABLE_DEV_LOGS = true;
-self.__WB_MANIFEST = [];
+
+/* eslint-disable no-restricted-globals */
+// The above comment disables ESLint warnings for this file only
 
 // Export default function for module requirements
 export default function noop() {
   return null;
 }
 
-// Prevent any service worker registration
-if (typeof self !== 'undefined') {
-  self.addEventListener = function() {};
+// Export mock service worker functions to satisfy CRA
+export function registerRoute() {
+  return null;
+}
+
+export function precacheAndRoute() {
+  return null;
+}
+
+export function createHandlerBoundToURL() {
+  return null;
+}
+
+export function getCacheKeyForURL() {
+  return null;
+}
+
+export function skipWaiting() {
+  return null;
+}
+
+export function clientsClaim() {
+  return null;
 }
